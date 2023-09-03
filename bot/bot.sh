bd22a4ae44cf73bf83cefad1c83a238b1e555958
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-09-03 12:53:48 +0700

    Update bot.sh

--- bot/bot.sh
+++ bot/bot.sh
@@ -1,67 +1,25 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipv4.icanhazip.com);
+ORANGE='\033[0;33m'
+MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-#link izin ip vps
-url_izin='https://raw.githubusercontent.com/rizkyckj/izin/master/izin'
 
-#IP VPS
-ip_vps=$(curl -sS ifconfig.me)
-
-# Mendapatkan isi file izin.txt dari URL
-izin=$(curl -s "$url_izin")
-
-# Memeriksa apakah konten izin.txt berhasil didapatkan
-if [[ -n "$izin" ]]; then
-  while IFS= read -r line; do
-    # Memisahkan nama VPS, IP VPS, dan tanggal kadaluwarsa
-    nama=$(echo "$line" | awk '{print $1}')
-    ipvps=$(echo "$line" | awk '{print $2}')
-    tanggal=$(echo "$line" | awk '{print $3}')
-
-    # Memeriksa apakah IP VPS saat ini cocok dengan IP VPS yang ada di izin.txt
-    if [[ "$ipvps" == "$ip_vps" ]]; then
-      echo "Nama VPS: $nama"
-      echo "IP VPS: $ipvps"
-      echo "Tanggal Kadaluwarsa: $tanggal"
-      break
-    fi
-  done <<< "$izin"
-
-  # Memeriksa apakah IP VPS ditemukan dalam izin.txt
-  if [[ "$ipvps" != "$ip_vps" ]]; then
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e ""
-    echo -e "            ${RED}PERMISSION DENIED !${NC}"
-    echo -e "   \033[0;33mYour VPS${NC} $ip_vps \033[0;33mHas been Banned${NC}"
-    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-    echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      \033[0;36mTelegram${NC} t.me/RVPNSTORES"
-    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6281935718766"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    exit 0
-  fi
-else
-  echo "Konten izin.txt tidak berhasil didapatkan dari URL"
-  exit 0
-fi
-
-clear
-echo -e "\033[96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e " \e[1;971m                      MENU BOT                \e[0m"
-echo -e "\033[96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "  ${grenbo}[1]${NC}${YELL} Add Bot Notifikasi${NC}"
-echo -e "  ${grenbo}[2]${NC}${YELL} Delete Bot Notifikasi${NC}"
+clear 
+echo -e "\033[1;93m┌─────────────────────────────┐\033[0m"
+echo -e "        BOT NOTIF           "
+echo -e "\033[1;93m└─────────────────────────────┘\033[0m"
+echo -e "" 
+echo -e "${ORANGE}[1].${NC}\033[0;36m Add Bot Notifikasi "
+echo -e "${ORANGE}[2].${NC}\033[0;36m Delete Bot Notifikasi"
 echo -e ""
-echo -e "  ${grenbo}[0]${NC}${YELL} Back To Menu${NC}"
+echo -e "\033[1;93m└─────────────────────────────┘\033[0m"
+echo -e "${ORANGE}  Press x or [ Ctrl+C ] • To-Exit${NC}"
 echo -e ""
-read -p "  Select From Options [ 1 - 2 or 0 ] : " menu
+read -p " Select menu : " opt
 echo -e ""
 case $opt in
-1) clear ; add-bot-notif ; exit ;;
-2) clear ; del-bot-notif ; exit ;;
-0) clear ; menu ; exit ;;
+1) clear ; add-bot-notif ;;
+2) clear ; del-bot-notif ;;
+0) clear ; menu ;;
 x) exit ;;
-*) echo "Anda salah tekan " ; sleep 1 ; menu ;;
+*) echo "Anda salah tekan" ; sleep 1 ; menu-domain ;;
 esac
