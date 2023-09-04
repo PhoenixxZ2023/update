98cd25298eea1a4bcb8ed62282be36e9e3c14c81
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-09-04 15:42:31 +0700

    Update trialvmess.sh

--- xray/trialvmess.sh
+++ xray/trialvmess.sh
@@ -23,13 +23,14 @@ export TIME="10"
 export URL="https://api.telegram.org/bot$KEY/sendMessage"
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 CITY=$(curl -s ipinfo.io/city )
+clear
 domain=$(cat /etc/xray/domain)
-clear
-IP=$(curl -sS ipv4.icanhazip.com)
-Login=Trial-`</dev/urandom tr -dc X-Z0-9 | head -c4`
-hari="1"
-Pass=1
-clear
+masaaktif=1
+Quota=5
+user=trial-`</dev/urandom tr -dc 0-9 | head -c3`
+clear 
+uuid=$(cat /proc/sys/kernel/random/uuid)
+exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
 tgl=$(date -d "$masaaktif days" +"%d")
 bln=$(date -d "$masaaktif days" +"%b")
 thn=$(date -d "$masaaktif days" +"%Y")
@@ -38,67 +39,159 @@ tgl2=$(date +"%d")
 bln2=$(date +"%b")
 thn2=$(date +"%Y")
 tnggl="$tgl2 $bln2, $thn2"
-useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
-exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
-hariini=`date -d "0 days" +"%Y-%m-%d"`
-expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
-echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
-echo -e "\033[1;93m◇━━━━━━━━━━━━━━━━━◇\033[0m"
-echo -e " Set Expired In Minutes           "
-echo -e "\033[1;93m◇━━━━━━━━━━━━━━━━━◇\033[0m"
-read -p " Menit : " pup
-CHATID="$CHATID"
-KEY="$KEY"
-TIME="$TIME"
-URL="$URL"
-TEXT="
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>      XRAY/VMESS</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Remarks   : ${user}
-Domain    : ${domain}
-Port TLS  : 400-900
-Port NTLS : 80, 8080, 8081-9999
-id        : ${uuid}
-alterId   : 0
-Security  : auto
-network   : ws or grpc
-Path      : /Multi-Path
-Dynamic   : https://bugmu.com/path
-Name      : vmess-grpc</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code> VMESS WS TLS</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>${vmesslink1}</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>VMESS WS NO TLS</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>${vmesslink2}</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code> VMESS gRPC</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>${vmesslink3}</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Format OpenClash :</code> <code>https://${domain}:81/vmess-$user.txt</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Expired Until  :</code> <code>$expe</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-"
+sed -i '/#vmess$/a\#vm# '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
+exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
+sed -i '/#vmessgrpc$/a\#vm# '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
+asu=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "443",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "/vmess",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "tls"
+}
+EOF`
+ask=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "80",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "/vmess",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "none"
+}
+EOF`
+grpc=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "443",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "grpc",
+      "path": "vmess-grpc",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "tls"
+}
+EOF`
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmess_base642=$( base64 -w 0 <<< $vmess_json2)
+vmess_base643=$( base64 -w 0 <<< $vmess_json3)
+vmesslink1="vmess://$(echo $asu | base64 -w 0)"
+vmesslink2="vmess://$(echo $ask | base64 -w 0)"
+vmesslink3="vmess://$(echo $grpc | base64 -w 0)"
+systemctl restart xray > /dev/null 2>&1
+service cron restart > /dev/null 2>&1
 
-curl -s --max-time $TIME -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
-echo ""
-mkdir -p /etc/vmess
+
+cat >/var/www/html/vmess-$user.txt <<-END
+
+◇━━━━━━━━━━━━━━━━━◇
+  G E O  P R O J E C T 
+◇━━━━━━━━━━━━━━━━━◇
+ https://github.com/wokszxd
+◇━━━━━━━━━━━━━━━━━◇
+# Format Vmess WS TLS
+
+- name: Vmess-$user-WS TLS
+  type: vmess
+  server: ${domain}
+  port: 443
+  uuid: ${uuid}
+  alterId: 0
+  cipher: auto
+  udp: true
+  tls: true
+  skip-cert-verify: true
+  servername: ${domain}
+  network: ws
+  ws-opts:
+    path: /vmess
+    headers:
+      Host: ${domain}
+
+# Format Vmess WS Non TLS
+
+- name: Vmess-$user-WS Non TLS
+  type: vmess
+  server: ${domain}
+  port: 80
+  uuid: ${uuid}
+  alterId: 0
+  cipher: auto
+  udp: true
+  tls: false
+  skip-cert-verify: false
+  servername: ${domain}
+  network: ws
+  ws-opts:
+    path: /vmess
+    headers:
+      Host: ${domain}
+
+# Format Vmess gRPC
+
+- name: Vmess-$user-gRPC (SNI)
+  server: ${domain}
+  port: 443
+  type: vmess
+  uuid: ${uuid}
+  alterId: 0
+  cipher: auto
+  network: grpc
+  tls: true
+  servername: ${domain}
+  skip-cert-verify: true
+  grpc-opts:
+    grpc-service-name: vmess-grpc
+
+◇━━━━━━━━━━━━━━━━━◇
+ Link Akun Vmess                   
+◇━━━━━━━━━━━━━━━━━◇
+Link TLS         : 
+${vmesslink1}
+◇━━━━━━━━━━━━━━━━━◇
+Link none TLS    : 
+${vmesslink2}
+◇━━━━━━━━━━━━━━━━━◇
+Link GRPC        : 
+${vmesslink3}
+◇━━━━━━━━━━━━━━━━━◇
+
+END
 if [ ! -e /etc/vmess ]; then
-    mkdir -p /etc/vmess
-    touch /etc/vmess/.vmess.db
+  mkdir -p /etc/vmess
 fi
-vms=$(cat /etc/vmess/.vmess.db)
-if [[ $vms = "" ]]; then
-    vm="0"
-else
-    vm=$(cat /etc/vmess/.vmess.db | grep "#vm#" | wc -l)
+
+if [ -z ${Quota} ]; then
+  Quota="0"
 fi
-echo ""
+
+c=$(echo "${Quota}" | sed 's/[^0-9]*//g');
+d=$(( ${c} * 1024*1024*1024 ));
+
+if [[ ${c} != "0" ]]; then
+echo "${d}" > /etc/vmess/${user}
+fi
+DATADB=$(cat /etc/vmess/.vmess.db | grep "^#vm#" | grep -w "${user}" | awk '{print $2}')
+if [[ "${DATADB}" != '' ]]; then
+  sed -i "/\b${user}\b/d" /etc/vmess/.vmess.db
+fi
 cat > /var/www/html/ssh-$Login.txt <<-END
 echo -e "\033[1;93m◇━━━━━━━━━━━━━━━━━◇\033[0m"
 echo -e " Xray/Vmess Account "
