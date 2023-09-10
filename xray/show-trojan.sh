0bffe813cc1dcffff629d31f91966a9a7b55514a
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-09-10 08:37:21 +0700

    Update show-trojan.sh

--- xray/show-trojan.sh
+++ xray/show-trojan.sh
@@ -13,7 +13,7 @@ jum=$(cat /etc/xray/config.json | grep -c '#!' | awk '{print $1/2}')
 if [[ $jum -gt 0 ]]; then
 exp=$(grep -wE "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
 echo -e "\e[33;1mUser\e[32;1m  : $user"
-echo -e "\e[33;1mLimit\e[32;1m : $lquota GB"
+echo -e "\e[33;1mLimit\e[32;1m : $wey GB"
 echo -e "\e[33;1mExp\e[32;1m   : $exp"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "slot" >> /root/.system
