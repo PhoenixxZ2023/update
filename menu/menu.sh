7ea6847dfc01228be464c981a9acf3f1f6d97fda
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-09-13 14:51:01 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,97 +1,19 @@
-
 #!/bin/bash
-#link izin ip vps
-url_izin='https://raw.githubusercontent.com/rizkyckj/izin/master/izin'
-
-#IP VPS
-ip_vps=$(curl -sS ifconfig.me)
-
-# Mendapatkan isi file izin.txt dari URL
-izin=$(curl -s "$url_izin")
-
-# Memeriksa apakah konten izin.txt berhasil didapatkan
-if [[ -n "$izin" ]]; then
-  while IFS= read -r line; do
-    # Memisahkan nama VPS, IP VPS, dan tanggal kadaluwarsa
-    nama=$(echo "$line" | awk '{print $1}')
-    ipvps=$(echo "$line" | awk '{print $2}')
-    tanggal=$(echo "$line" | awk '{print $3}')
-
-    # Memeriksa apakah IP VPS saat ini cocok dengan IP VPS yang ada di izin.txt
-    if [[ "$ipvps" == "$ip_vps" ]]; then
-      echo "Nama VPS: $nama"
-      echo "IP VPS: $ipvps"
-      echo "Tanggal Kadaluwarsa: $tanggal"
-      break
-    fi
-  done <<< "$izin"
-
-  # Memeriksa apakah IP VPS ditemukan dalam izin.txt
-  if [[ "$ipvps" != "$ip_vps" ]]; then
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e ""
-    echo -e "            ${RED}PERMISSION DENIED !${NC}"
-    echo -e "   \033[0;33mYour VPS${NC} $ip_vps \033[0;33mHas been Banned${NC}"
-    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-    echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      \033[0;36mTelegram${NC} t.me/RVPNSTORES"
-    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6281935718766"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    exit 0
-  fi
-else
-  echo "Konten izin.txt tidak berhasil didapatkan dari URL"
-  exit 0
-fi
-
-cek=$(service ssh status | grep active | cut -d ' ' -f5)
-if [ "$cek" = "active" ]; then
-stat=-f5
-else
-stat=-f7
-fi
-ssh=$(service ssh status | grep active | cut -d ' ' $stat)
-if [ "$ssh" = "active" ]; then
-ressh="${BBlue}ON${NC}"
-else
-ressh="${red}OFF${NC}"
-fi
-ngx=$(service nginx status | grep active | cut -d ' ' $stat)
-if [ "$ngx" = "active" ]; then
-resngx="${BBlue}ON${NC}"
-else
-resngx="${red}OFF${NC}"
-fi
-dbr=$(service dropbear status | grep active | cut -d ' ' $stat)
-if [ "$dbr" = "active" ]; then
-resdbr="${BBlue}ON${NC}"
-else
-resdbr="${red}OFF${NC}"
-fi
-v2r=$(service xray status | grep active | cut -d ' ' $stat)
-if [ "$v2r" = "active" ]; then
-resv2r="${BBlue}ON${NC}"
-else
-resv2r="${red}OFF${NC}"
-fi
-# STATUS SERVICE UDP CUSTOM
-if [[ $udpc == "running" ]]; then
-   udp2="${GB}[ ON ]${NC}${NC}"
-else
-   udp2="${RB}[ OFF ]${NC}"
-fi
+# COLOR VALIDATION
+clear
 RED='\033[0;31m'
-NC='\033[0m'
+NC='\033[36m'
 gray="\e[1;30m"
 Blue="\033[36m"
 GREEN='\033[0;32m'
 grenbo="\e[92;1m"
 YELL='\033[0;33m'
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 ) 
-CITY=$(curl -s ipinfo.io/city )
+BGX="\033[42m"
+UPDATE="https://raw.githubusercontent.com/rizkihdyt6/scupdate/rizki/"
+BOT="https://raw.githubusercontent.com/rizkihdyt6/scupdate/rizki/"
+ISP=$(cat /etc/xray/isp)
 NS=$(cat /etc/xray/dns)
+CITY=$(cat /etc/xray/city)
 IPVPS=$(curl -s ipv4.icanhazip.com)
 domain=$(cat /etc/xray/domain)
 RAM=$(free -m | awk 'NR==2 {print $2}')
@@ -100,8 +22,8 @@ MEMOFREE=$(printf '%-1s' "$(free -m | awk 'NR==2{printf "%.2f%%", $3*100/$2 }')"
 LOADCPU=$(printf '%-0.00001s' "$(top -bn1 | awk '/Cpu/ { cpu = "" 100 - $8 "%" }; END { print cpu }')")
 MODEL=$(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')
 CORE=$(printf '%-1s' "$(grep -c cpu[0-9] /proc/stat)")
-Exp="Lifetime "
-Name="RVPN"
+Exp="LIFETIME"
+Name="RizkiHdyt | Muslihudin"
 DATEVPS=$(date +'%d/%m/%Y')
 TIMEZONE=$(printf '%(%H:%M:%S)T')
 SERONLINE=$(uptime -p | cut -d " " -f 2-10000)
@@ -158,54 +80,114 @@ else
     ssh=$(cat /etc/ssh/.ssh.db | grep "###" | wc -l)
 fi
 clear
-echo -e "\033[1;93m──────────────────────────────────────────\033[0m"
-echo -e "\033[42m    ∆ INFORMATION AUTOSCRIPT VPS ∆                \033[0m"
-echo -e "\033[1;93m──────────────────────────────────────────\033[0m"
-echo -e " ${YELL}System OS${NC}     : \033[0;32m$MODEL${NC}"
-echo -e " ${YELL}Server RAM${NC}    : \033[0;32m$RAM MB $NC"
-echo -e " ${YELL}Usage RAM${NC}     : \033[0;32m$USAGERAM MB $NC"
-echo -e " ${YELL}Usage Memory${NC}  : \033[0;32m$MEMOFREE${NC}"
-echo -e " ${YELL}LoadCPU${NC}       : \033[0;32m$LOADCPU%${NC}"
-echo -e " ${YELL}Uptime Server${NC} : \033[0;32m$SERONLINE${NC}"
-echo -e " ${YELL}Core System${NC}   : \033[0;32m$CORE${NC}"
-echo -e " ${YELL}Date${NC}          : \033[0;32m$DATEVPS${NC}"
-echo -e " ${YELL}Time${NC}          : \033[0;32m$TIMEZONE${NC}"
-echo -e " ${YELL}Isp VPS${NC}       : \033[0;32m$ISP${NC}"
-echo -e " ${YELL}City${NC}          : \033[0;32m$CITY${NC}"
-echo -e " ${YELL}IP VPS${NC}        : \033[0;32m$IPVPS${NC}"
-echo -e " ${YELL}Domain${NC}        : \033[0;32m$domain${NC}"
-echo -e " ${YELL}NS Domain${NC}     : \033[0;32m$NS${NC}"
-echo -e " ${YELL}Exp Script${NC}    : \033[0;32m$Exp${NC}"
-echo -e " ${YELL}Name Author${NC}   : \033[0;32m$Name${NC}"
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
-echo -e "\033[1;93m│\033[0m ${RED}SSH  VMESS   VLESS  TROJAN   SHADOWSOCKS$NC"
-echo -e "\033[1;93m│\033[0m ${Blue} $ssh     $vm       $vl      $tr           $ss   $NC"
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"  
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
-echo -e "\033[1;93m│  ${grenbo}1.${NC} \033[0;36mSSH OVPN MANAGER${NC} ${grenbo}4.${NC} \033[0;36mTROJAN MANAGER${NC}"
-echo -e "\033[1;93m│  ${grenbo}2.${NC} \033[0;36mVMESS MANAGER${NC}    ${grenbo}5.${NC} \033[0;36mSHDWSK MANAGER${NC}"
-echo -e "\033[1;93m│  ${grenbo}3.${NC} \033[0;36mVLESS MANAGER${NC}    ${grenbo}6.${NC} \033[0;36mOTHER SETTING${NC}"
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[36m┌──────────────────────────────────────────┐\033[36m"
+echo -e "\033[36m│           ∆ NEWBIE PROJECT VPN  ∆        │\033[36m"
+echo -e "\033[36m└──────────────────────────────────────────┘\033[36m"
+echo -e "\033[36m┌── RAM ──┐      ┌── CPU ──┐      ┌─MEMORY─┐\033[36m"
+echo -e "\033[0;32m   $USAGERAM MB            $LOADCPU%             $MEMOFREE% "
+echo -e "\033[36m└─────────┘      └─────────┘      └────────┘\033[36m"
+echo -e "\033[36m┌──────────────── \e[92;1mINFORMATION\033[36m ──────────────┐\033[36m"
+echo -e "\033[36m ${Blue}System OS${NC}     : \033[36m$MODEL${NC}"
+echo -e "\033[36m ${Blue}Server RAM${NC}    : \033[36m$RAM MB $NC"
+echo -e "\033[36m ${Blue}Uptime Server${NC} : \033[36m$SERONLINE${NC}"
+echo -e "\033[36m ${Blue}Core System${NC}   : \033[36m$CORE${NC}"
+echo -e "\033[36m ${Blue}Date${NC}          : \033[36m$DATEVPS${NC}"
+echo -e "\033[36m ${Blue}Time${NC}          : \033[36m$TIMEZONE${NC}"
+echo -e "\033[36m ${Blue}Domain${NC}        : \033[36m$domain${NC}"
+echo -e "\033[36m ${Blue}NS Domain${NC}     : \033[36m$NS${NC}"
+echo -e "\033[36m└──────────────────────────────────────────┘\033[36m"
+
+echo -e "\033[36m   ┌─────────── \e[92;1mACTIVE ACCOUNT\033[36m ──────────┐\033[0m"
+echo -e "\033[36m       \033[0m ${Blue}SSH         :$NC${grenbo} $ssh ACCOUNT"
+echo -e "\033[36m       \033[0m ${Blue}VMESS       :$NC${grenbo} $vm ACCOUNT"
+echo -e "\033[36m       \033[0m ${Blue}VLESS       :$NC${grenbo} $vl ACCOUNT"
+echo -e "\033[36m       \033[0m ${Blue}TROJAN      :$NC${grenbo} $tr ACCOUNT"
+echo -e "\033[36m       \033[0m ${Blue}SHADOWSOCKS :$NC${grenbo} $ss ACCOUNT"
+echo -e "\033[36m   └─────────────────────────────────────┘\033[0m"
+echo -e "\033[36m┌────────────── \e[92;1mCREATE ACCOUNT\033[36m ────────────┐\033[0m"
+echo -e "\033[36m   ${grenbo}1.${NC} \033[0;36mSSH&OVPN ${NC}"
+echo -e "\033[36m   ${grenbo}2.${NC} \033[0;36mVMESS${NC}"
+echo -e "\033[36m   ${grenbo}3.${NC} \033[0;36mVLESS${NC}"
+echo -e "\033[36m   ${grenbo}4.${NC} \033[0;36mTROJAN${NC}"
+echo -e "\033[36m   ${grenbo}5.${NC} \033[0;36mSS${NC}"
+echo -e "\033[36m└──────────────────────────────────────────┘\033[0m"
 echo -e ""
-read -p "        Select From Options [ 1 - 6 ] : " menu
+echo -e "\033[36m┌────────────────── \e[92;1mOTHER\033[36m ──────────────────┐\033[0m"
+echo -e "\033[36m   ${grenbo}6. ${NC} \033[0;36mRUNNING SYSTEM${NC}"
+echo -e "\033[36m   ${grenbo}7. ${NC} \033[0;36mBACKUP & RESTORE${NC}"
+echo -e "\033[36m   ${grenbo}8. ${NC} \033[0;36mPORT VPS INFO${NC}"
+echo -e "\033[36m   ${grenbo}9. ${NC} \033[0;36mLOAD VPS INFO${NC}"
+echo -e "\033[36m   ${grenbo}10.${NC} \033[0;36mSPEEDTEST${NC}"
+echo -e "\033[36m   ${grenbo}11.${NC} \033[0;36mCHANGE DOMAIN${NC}"
+echo -e "\033[36m   ${grenbo}12.${NC} \033[0;36mCHANGE BANNER${NC}"
+echo -e "\033[36m   ${grenbo}13.${NC} \033[0;36mRESTART SERVICE${NC}"
+echo -e "\033[36m   ${grenbo}14.${NC} \033[0;36mRESTART SERVER${NC}"
+echo -e "\033[36m   ${grenbo}15.${NC} \033[0;36mAUTO KILL${NC}"
+echo -e "\033[36m   ${grenbo}16.${NC} \033[0;36mAUTO REBOOT${NC}"
+echo -e "\033[36m   ${grenbo}17.${NC} \033[0;36mUPDATE SCRIPT${NC}"
+echo -e ""
+echo -e "\033[36m└──────────────────────────────────────────┘\033[0m"
+echo -e ""
+echo -e "\033[36m┌────────────── \e[92;1mNEWBIE TUNNEL\033[36m ─────────────┐\033[0m"
+echo -e "\033[36m   ${Blue}IP VPS${NC}        : \033[0;32m$IPVPS${NC}"
+echo -e "\033[36m   ${Blue}Exp Script${NC}    : \033[0;32m$Exp${NC}"
+echo -e "\033[36m   ${Blue}Name Author${NC}   : \033[0;32m$Name${NC}"
+echo -e "\033[36m└──────────────────────────────────────────┘\033[0m"
+echo -e ""
+read -p "Select From Options [ 1 - 18 ] : " menu
 case $menu in
 1)
-    menu-ssh
+    ssh
     ;;
 2)
-    menu-vmess
+    vmess
     ;;
 3)
-    menu-vless
+    vless
     ;;
 4)
-    menu-trojan
+    trojan
     ;;
 5)
-    menu-shadowsocks
+    shadowsocks
     ;;
 6)
-    menu-set
+    run
+    ;;
+7)
+    menu-backup
+    ;;
+8)
+    portin
+    ;;
+9)
+    gotop
+    ;;
+10)
+    clear
+    speedtest
+    ;;
+11)
+    get-domain
+    ;;
+12)
+    nano /etc/banner
+    ;;
+13)
+    seres
+    ;;
+14)
+    reboot
+    ;;
+
+15)
+    autokill
+    ;;
+16)
+    auto-reboot
+    ;;
+17)
+    wget ${UPDATE}update.sh && chmod +x update.sh && ./update.sh
     ;;
 *)
     menu
